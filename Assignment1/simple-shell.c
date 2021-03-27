85d40c7ca252a6e429eac98bdd3aff28e02af0a1
d_PITHECUSE_b <gihong96@gmail.com>
2021-03-27 23:18:41 +0900

    complete the command execution function

--- Assignment1/simple-shell.c
+++ Assignment1/simple-shell.c
@@ -13,6 +13,9 @@ CHANGES :
   NO   VERSION   DATE         WHO           DETAIL
   1    1.1       24 Mar 2021  Hong Geun Ji  Change the way to print the prompt string
   2    1.2       25 Mar 2021  Hong Geun Ji  Complete the command parsing
+  3    1.3       26 Mar 2021  Hong Geun Ji  Separate the parsing function
+  4    1.4       26 Mar 2021  Hong Geun Ji  Add forking with the special symbols such as '|', '>'
+  5    1.5       27 Mar 2021  Hong Geun Ji  Error handling with exec functions
 */
 
 #include <stdio.h>
@@ -22,11 +25,12 @@ CHANGES :
 #include <sys/wait.h>
 #include <fcntl.h>
 
-#define MAX_LINE 20 // the maximum length command
+#define SHELL_PROMPT "JI_SH> "  // default shell prompt
+#define MAX_LINE 100 // the maximum length command
 #define TRUE 1
 #define FALSE 0
-#define SHELL_PROMPT "osh> "
-
+#define	WRITE_END	1
+#define	READ_END	0
 #define NORMAL_MODE 0 // execution modes
 #define REDIRECTION_MODE_IN 1
 #define REDIRECTION_MODE_OUT 2
@@ -34,9 +38,10 @@ CHANGES :
 
 // the length of the prompt string
 // need to change this as well if the prompt string is changed
-#define PROMPT_BUFF_SIZE 6
+#define PROMPT_BUFF_SIZE 7
 
-int Execution(char** args, int n, int mode);  // fork by mode
+int MyParse(char* usr_cmd, char** args, int* background, int* mode);
+int Execution(char** args, int mode, int background, int symbol_loc);  // fork by mode
 
 int main(void)
 {
@@ -47,9 +52,10 @@ int main(void)
   int mode;        // execution mode when fork
   int i;          // index for the args
   int cmd_len;    // the number of chars from user command input
-  int args_len;   // the number of char* elements from args
+  int special_symbol_loc; // the symbol location such as '|' or '>'
+  int background;
 
-  // prompt runs until it meets the end moment
+  // prompt runs until it is killed by something or someone
   shell_is_alive = TRUE;
   while(shell_is_alive)
   {
@@ -61,71 +67,40 @@ int main(void)
       fprintf(stderr, "prompt error\n\n");
       exit(EXIT_FAILURE);
     }
+
+    memset(usr_cmd, '\0', MAX_LINE);
     if((cmd_len = read(STDIN_FILENO, usr_cmd, MAX_LINE+1)) > MAX_LINE)
     {
       // use stderr file to let fprintf prints error right away
       fprintf(stderr, "command error! may be too long?\n\n");
       exit(EXIT_FAILURE);
     }
-    *(usr_cmd+cmd_len-1) = '\0'; // swap the '\n' to null character
+
+    if(*usr_cmd == '\n') continue;  // do nothing with the single newline
+    *(usr_cmd+cmd_len-1) = '\0';    // swap the '\n' to null character
+    if(strcmp(usr_cmd, "exit\0") == 0) break; // exit condition
+
     /**************** user command input area *****************/
 
-
-    /**************** command parsing area *****************/
-    i = 0;
-    *args = strtok(usr_cmd, " ");
-    while(*(args+i))
+    special_symbol_loc = MyParse(usr_cmd, args, &background, &mode);
+    if(Execution(args, mode, background, special_symbol_loc) == 0) continue;    // fork followed by mode
+    else
     {
-      i++;
-      *(args+i) = strtok(NULL, " ");  // parse the next location from right before
+      printf("something went wrong...\n");
+      shell_is_alive = FALSE; // kill this shell process on execution error
     }
-    args_len = i; // save the number of agrs elements
-    /**************** command parsing area *****************/
-
-    /**************** fork area *****************/
-    i = 0;
-    mode = NORMAL_MODE;
-    while(*(args+i))  // choose mode from args
-    {
-      if(strcmp(*(args+i), ">") == 0)  // we found there is a file redirection (output)
-      {
-        mode = REDIRECTION_MODE_OUT;
-        strcpy("", *(args+i));
-      }
-      else if(strcmp(*(args+i), "<") == 0) // we found there is a file redirection (input)
-      {
-        mode = REDIRECTION_MODE_IN;
-        strcpy("", *(args+i));
-      }
-      Execution(args, args_len, mode);
-      i++;
-    }
-    /**************** fork area *****************/
-
   } // while
 
   return 0;
-  // todo: test git push
 }
 
-int Execution(char** args, int n, int mode)
+int Execution(char** args, int mode, int background, int symbol_loc)
 {
-  int fd;
-  int fd_changed = FALSE;
-  if(mode == REDIRECTION_MODE_OUT)
-  {
-      fd = open(*(args+n-1), O_CREAT|O_RDWR|O_TRUNC);
-      dup2(fd, STDOUT_FILENO);
-      fd_changed = TRUE;
-  }
-  if(mode == REDIRECTION_MODE_IN)
-  {
-      fd = open(*(args+n-1), O_CREAT|O_RDWR);
-      dup2(fd, STDIN_FILENO);
-      fd_changed = TRUE;
-  }
+  pid_t pid;  // process id for non PIPE_MODE
+  pid_t pid2; // process id for PIPE_MODE
+  int status;
 
-  pid_t pid = fork();
+  pid = fork();
   if(pid < 0) // error
   {
     fprintf(stderr, "Fork failed");
@@ -133,12 +108,117 @@ int Execution(char** args, int n, int mode)
   }
   else if(pid == 0) // child
   {
+    int fd;
+    int fdl[2];
+
+    // use redirection according to the mode
+    if(mode == REDIRECTION_MODE_OUT)
+    {
+        printf("%s\n",*(args+symbol_loc+1));
+        fd = open(*(args+symbol_loc+1),
+                  O_CREAT|O_RDWR|O_TRUNC,
+                  0666);
+        dup2(fd, STDOUT_FILENO);
+    }
+    if(mode == REDIRECTION_MODE_IN)
+    {
+        fd = open(*(args+symbol_loc+1),
+                  O_CREAT|O_RDWR,
+                  0666);
+        dup2(fd, STDIN_FILENO);
+    }
+
+    // child use pipe on PIPE_MODE
+    if(mode == PIPE_MODE)
+    {
+      pipe(fdl);
+      pid2 = fork();
+
+      if(pid2 < 0) // error
+      {
+        fprintf(stderr, "Fork2 failed");
+        return 1;
+      }
+      else if(pid2 == 0) // child again
+      {
+        dup2(fdl[READ_END], STDIN_FILENO);  // take the result from the parent
+        close(fdl[WRITE_END]);
+        execvp(*(args+symbol_loc+1), args+symbol_loc+1);  // execute the right half of args (after the pipe symbol)
+        exit(1);  // exit on pipe error
+      }
+      else  // child as a parent
+      {
+        dup2(fdl[WRITE_END], STDOUT_FILENO);  // give the result to the child
+        close(fdl[READ_END]);
+        sleep(1); // give some time to ready for next command
+      }
+    }
     execvp(*args, args);
-    printf("execvp call was unsuccessful\n");
-  }
+    exit(1);  // exit on fail (such as meaningless command)
+  } // else if
   else  // parent
   {
-    waitpid(pid, NULL, 0);
-  }
+    if(background) {
+      fprintf(stdout, "[%d]\n", pid);
+      fflush(stdout);
+    }
+    else{
+      waitpid(pid, &status, 0);
+    }
+  } // else
   return 0;
+}
+
+int MyParse(char* usr_cmd, char** args, int* background, int* mode)
+{
+  int i = 0;
+  int symbol_loc = 0;
+  int args_len;
+
+  // save the each token to args
+  *args = strtok(usr_cmd, " ");
+  while(*(args+i))
+  {
+    i++;
+    *(args+i) = strtok(NULL, " ");  // parse the next location from right before
+  }
+  args_len = i; // save the number of agrs elements
+
+  i = 0;
+  *mode = NORMAL_MODE;
+  *background = FALSE;
+
+  // check whether the command should run on background or not
+  if(strcmp(*(args+args_len-1), "&") == 0) {
+    *background = TRUE;
+    *(args+args_len-1) = NULL;
+  }
+
+  while(*(args+i))  // choose the mode from here (REDIRECTION_MODE or NORMAL_MODE)
+  {
+    // printf("!%s!\n", *(args+i));  // testing what tokens are
+    if(strcmp(*(args+i), ">") == 0)  // we found there is a file redirection (output)
+    {
+      *mode = REDIRECTION_MODE_OUT;
+      *(args+i) = NULL;
+      symbol_loc = i;
+      break;
+    }
+    if(strcmp(*(args+i), "<") == 0) // we found there is a file redirection (input)
+    {
+      *mode = REDIRECTION_MODE_IN;
+      *(args+i) = NULL;
+      symbol_loc = i;
+      break;
+    }
+    if(strcmp(*(args+i), "|") == 0)
+    {
+      *mode = PIPE_MODE;
+      *(args+i) = NULL;
+      symbol_loc = i;
+      break;
+    }
+    i++;
+  }
+  return symbol_loc;
 }
