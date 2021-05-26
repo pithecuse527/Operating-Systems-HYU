3a0f83f66ad5bd8f5901ed33335191499483b3dc
pithecuse527 <gihong96@gmail.com>
2021-05-26 06:04:45 +0000

    thread pool implement v.1

--- Assignment4/threadpool.c
+++ Assignment4/threadpool.c
@@ -4,6 +4,7 @@
  */
 #include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <fcntl.h>           /* For O_* constants */
 #include <sys/stat.h>        /* For mode constants */
 #include <semaphore.h>
@@ -27,6 +28,7 @@ typedef struct {
  * 스레드 풀의 FIFO 대기열인 worktodo 배열로 원형 버퍼의 역할을 한다.
  */
 static task_t worktodo[QUEUE_SIZE];
+int head = 0, tail = 0;
 
 /*
  * mutex는 대기열을 조회하거나 변경하기 위해 사용하는 상호배타 락이다.
@@ -39,6 +41,18 @@ static pthread_mutex_t mutex;
  */
 static int enqueue(task_t t)
 {
+    pthread_mutex_lock(&mutex); // lock before push
+    int push_loc = (head+1)%QUEUE_SIZE;
+    
+    if(push_loc == tail) {  // if the queue is full
+        pthread_mutex_unlock(&mutex);   // unlock before return
+        return 1;
+    }
+    
+    worktodo[push_loc] = t; // push t
+    head = push_loc;
+    pthread_mutex_unlock(&mutex);
+    return 0;
 }
 
 /*
@@ -47,6 +61,21 @@ static int enqueue(task_t t)
  */
 static int dequeue(task_t *t)
 {
+    pthread_mutex_lock(&mutex); // lock before pop
+    int pop_loc = (tail+1)%QUEUE_SIZE;
+    
+    if(head == tail) {   // if the queue is empty
+        pthread_mutex_unlock(&mutex);   // unlock before return
+        return 1;
+    }
+    
+    // saves the worktodo's info. (pop)
+    t -> function = worktodo[pop_loc].function;
+    t -> data = worktodo[pop_loc].data;
+    tail = (tail+1)%QUEUE_SIZE;
+    pthread_mutex_unlock(&mutex);
+    
+    return 0;
 }
 
 /*
@@ -54,13 +83,20 @@ static int dequeue(task_t *t)
  * 세마포 sem은 카운팅 세마포로 그 값은 대기열에 입력된 작업의 갯수를 나타낸다.
  */
 static pthread_t bee[NUMBER_OF_BEES];
-static sem_t *sem;
+static sem_t sem;
 
 /*
  * 풀에 있는 일꾼 스레드로 FIFO 대기열에서 기다리고 있는 작업을 하나씩 꺼내서 실행한다.
  */
 static void *worker(void *param)
 {
+    task_t* work = (task_t*)malloc(sizeof(task_t));
+    while(1) {
+        sem_wait(&sem);
+        if(dequeue(work) == 0) {    // try dequeue
+            (*(work->function))(work->data);    // let the bee do its job
+        }
+    }
 }
 
 /*
@@ -69,6 +105,15 @@ static void *worker(void *param)
  */
 int pool_submit(void (*f)(void *p), void *p)
 {
+    task_t *task = (task_t*)malloc(sizeof(task_t));
+    task -> function = f;      // function to be executed
+    task -> data = p;          // data as a parameter
+    
+    if(enqueue(*task) != 0) // try enqueue
+        return 1;
+    
+    sem_post(&sem);
+    return 0;   // on success
 }
 
 /*
@@ -76,6 +121,15 @@ int pool_submit(void (*f)(void *p), void *p)
  */
 void pool_init(void)
 {
+    int i;  // bee init.
+    for(i=0; i<NUMBER_OF_BEES; i++) {
+        pthread_create(&bee[i], NULL, worker, NULL);
+    }
+    
+    // mutex and semaphore init
+    pthread_mutex_init(&mutex, NULL);
+    sem_init(&sem, 0, 0);
+
 }
 
 /*
@@ -83,4 +137,12 @@ void pool_init(void)
  */
 void pool_shutdown(void)
 {
+    int i;
+    for(i=0; i<NUMBER_OF_BEES; i++) {
+        pthread_cancel(bee[i]);
+        pthread_join(bee[i], NULL);
+    }
+    
+    sem_destroy(&sem);
+    pthread_mutex_destroy(&mutex);
 }
